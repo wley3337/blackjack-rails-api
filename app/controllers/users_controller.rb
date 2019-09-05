70b2d70b775c11931dfde11972e3047072076772
wley3337 <wley3337@gmail.com>
2019-09-05 07:14:35 -0400

    user creation working with react front, generating token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < ApplicationController
+
     skip_before_action :authenticate, only: [:create, :log_in]
 
 
     def create
-        byebug
-        #create new user
-
         user = User.new(
             first_name: create_user_params[:firstName],
             last_name: create_user_params[:lastName],
@@ -14,15 +12,13 @@ class UsersController < ApplicationController
             )
         user.bank = 1000
         user.save
-        byebug
         #generate token
         token =  generate_token(user)
 
-        render json: {succes: true, user: user.to_json(only: [:first_name, :last_name, :user_name, :bank]), token: token}
+        render json: {succes: true, user: user.to_json(only: [:first_name, :last_name, :username, :bank]), token: token}
     end 
     
     def log_in
-        byebug
         #local strong params
         login_params = params.require(:user).permit(:username, :password)
         user = User.find_by(username: login_params["username"])
@@ -43,12 +39,4 @@ class UsersController < ApplicationController
         params.require(:user).permit(:firstName, :lastName, :username, :password)
     end 
 
-    # User Schema :
-    #     t.string "first_name"
-    #     t.string "last_name"
-    #     t.string "username"
-    #     t.integer "bank"
-    #     t.datetime "created_at", precision: 6, null: false
-    #     t.datetime "updated_at", precision: 6, null: false
-    #   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
     has_many :user_hands
     has_many :rounds, through: :user_hands
     has_secure_password
+    # User Schema :
+    #     t.string "first_name"
+    #     t.string "last_name"
+    #     t.string "username"
+    #     t.integer "bank"
+    #     t.datetime "created_at", precision: 6, null: false
+    #     t.datetime "updated_at", precision: 6, null: false
+    #   end
 end
