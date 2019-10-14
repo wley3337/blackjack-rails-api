3f9bbe9ece94c9665760ec62beb615bd8fbaa416
wley3337 <wley3337@gmail.com>
2019-10-14 09:51:18 -0400

    added factory_bot_rails and set-up first test using it.

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+    factory :user do 
+        first_name {"U1"}
+        last_name {"U1Last"}
+        bank {1000}
+        username {"u1username"}
+    end 
+  end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:context) do
+    @u1 = build(:user)
+  end 
+  
+  after(:context) do 
+    
+  end 
+  
+  context "#serialize_user" do 
+  it "correctly serializes a user" do 
+      output = { firstName: "U1", lastName: "U1Last", bank: 1000, username: "u1username"}
+      expect(@u1.serialize_user).to eq(output)
+    end 
+  end 
+
+  # context "#wins_losses" do 
+  #   xit "returns a hash of reduces wins and losses" do 
+   
+      
+  #     output = {wins: 2, losses: 2, bets: 1700}
+  #     allow(@u1).to receive(:user_hands).and_return(users_hands)
+  #     expect(@u1.wins_losses).to eq(output)
+
+  #   end 
+  # end 
 end
